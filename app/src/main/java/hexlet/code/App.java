9bed405841124b5b576050b5ea3583488a6c1616
Sergey Ivanov <beckron@mail.ru>
2025-01-21 19:37:02 +0300

    testing deploy

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -65,6 +65,7 @@ public class App {
     public static Javalin getApp() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
         var dataBaseUrl = getJdbcUrl();
+        System.out.println(JDBC_DATABASE_URL);
         if (dataBaseUrl == null || dataBaseUrl.equals(DEFAULT_JDBC_URL)) {
             hikariConfig.setJdbcUrl(dataBaseUrl);
         } else {
